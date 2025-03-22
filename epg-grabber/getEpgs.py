22622f2daf552d0600954bec78220f990ae5d753
Dias2001 <49402983+Dias2001@users.noreply.github.com>
2025-03-21 22:29:47 -0400

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -69,10 +69,11 @@ def filter_and_build_epg(urls):
 	    
 m3u4u_epg = os.getenv("M3U4U_EPG")
 urls = [
-        'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
-        'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
 	'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
 	'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz',
+	'https://epgshare01.online/epgshare01/epg_ripper_PLEX1.xml.gz',
 ]
 
 if __name__ == "__main__":
